a32b1860183b1d1c7a906ae84b82e02fccf5cbaa
jforeman7 <31979772+jforeman7@users.noreply.github.com>
2017-11-29 20:31:01 -0800

    Update MST.cpp

--- MST.cpp
+++ MST.cpp
@@ -49,9 +49,60 @@ class Graph
 		// Create a Priority Queue to do automatic sorting of the vertices.
 		priority_queue<Vertex, vector<Vertex>, greater<Vertex> > queue;
 		
-		// Keep track of the keys in a vector, and set a sentinel value of 9999, which will be greater than all possible values.
+		// Keep track of the keys in a vector, and set a sentinel value of the largest possible int, 
+		// which will be greater than all possible values used in the graph.
 		vector<int> key(V, numeric_limits<int>::max());
-	}
+		
+		// Keeps track of the parents, with a sentinel value of -1 in the beginning.
+		vector<int> parent(V, -1);
+		
+		// Keeps track if a vertex is in the MST.
+		vector<bool> included(V, false);
+		
+		// Place the root into the queue to begin the algorithm.
+		queue.push(make_pair(0, root));
+		key[root] = 0;
+		
+		while(!queue.empty())
+		{
+			// Extract the min.
+			int u = queue.top().second;
+			queue.pop();
+			
+			// Vertex u is now in the MST.
+			included[u] = true;
+			
+			// Use the STL iterator to get all of the  adjacent vertices to u.
+			list<pair<int,int> >::iterator i;
+			
+			// Do an STL for loop.
+			// Use pre-iterator to be able to get the end().
+			for(i = adjacencyList[u].begin(); i != adj[u].end(); ++i)
+			{
+				// Get the vertex-weight pair.
+				int v = (*i).first;
+				int w = (*i).second;
+				
+				// If the vertex is not in the MST, and is smaller, place in MST.
+				if(key[v] > weight && included[v] == false)
+				{
+					// Assign the weight associated with the vertex.
+					key[v] = w;
+					queue.push(make_pair(key[v], v));
+					
+					// Since this was found from u, u is the parent of v.
+					parent[v] = u;
+				} // End if.
+				
+			} // End i loop.
+			
+		} // End while loop.
+		
+		// Print out the results.
+		for(int i = 1; i < V; ++i)
+			cout << parent[i] << endl;
+		
+	} // End trim().
 };
 
 int main()
