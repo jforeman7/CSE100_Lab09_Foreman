7c8248b1c50f7fe1214a8766c4957a9227edd6e1
jforeman7 <31979772+jforeman7@users.noreply.github.com>
2017-11-29 19:33:44 -0800

    Update MST.cpp

--- MST.cpp
+++ MST.cpp
@@ -2,4 +2,20 @@
 // CSE 100 03L
 // Lab 09: MST
 
-#include<iostream>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Number of vertices in the graph.
+	int V;
+	
+	// Number of edges in the graph.
+	int E;
+	
+	// Take in the number of vertices and edges for the graph.
+	cin >> V >> E;
+	
+	return 0;	
+}
